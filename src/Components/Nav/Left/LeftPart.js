fe66de26663fa2f0ac335f019ed54898afb15828
Narmadhu <oknarmadhu10496@gmail.com>
2021-07-26 22:02:08 +0530

    added some styling for Nav

--- src/Components/Nav/Left/LeftPart.js
+++ src/Components/Nav/Left/LeftPart.js
@@ -1,17 +1,20 @@
 import React from "react";
 // import FilterIcon from "@material-ui/icons/";
 import CachedIcon from '@material-ui/icons/Cached';
+import Button from '@material-ui/core/Button';
+import "./LeftPart.css";
 
 function LeftPart() {
   return (
     <div className="flex-row">
       <h3>Tickets</h3>
       <div>
-      <button>all</button>
-      <button>only my tickets</button>
-      <button>recently updated</button>
+      <Button style={{backgroundColor:"green",color:"white",marginLeft:"3px" }} >all</Button>
+      <Button>only my tickets</Button>
+      <Button>recently updated</Button>
       {/* <button><FilterIcon /></button> */}
-      <button><CachedIcon /></button>
+      <Button endIcon={<CachedIcon />} />
+      <Button endIcon={<CachedIcon />} />
       </div>
     </div>
   );
--- src/Components/Nav/Right/RightPart.js
+++ src/Components/Nav/Right/RightPart.js
@@ -4,11 +4,14 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import SettingsIcon from '@material-ui/icons/Settings';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
 function RightPart() {
   return (
     <div>
-        <Button endIcon={<SearchIcon />}><input type="text" placeholder="search"  /></Button>
+
+        {/* <Button endIcon={<SearchIcon />}><input type="text" placeholder="search"  /></Button> */}
+        <Input placeholder="search" type="text" /><Button endIcon={<SearchIcon />} />
         <Button startIcon={<SettingsIcon />}>Configuration</Button>
         <span>(0-30)</span>
         <Button endIcon={<ArrowForwardIosIcon />}  startIcon={<ArrowBackIosIcon />} />

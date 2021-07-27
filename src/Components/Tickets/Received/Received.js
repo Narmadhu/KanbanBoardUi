c8b9f323064051cd15034331b3f169e1b1a428a6
Narmadhu <oknarmadhu10496@gmail.com>
2021-07-27 09:34:00 +0530

    created the received orders with styling

--- src/Components/Tickets/Received/Received.js
+++ src/Components/Tickets/Received/Received.js
@@ -5,37 +5,39 @@ function Received() {
     <div className="grid-column" >
       <div className="each-grid">
         <span>#26 <button>Response due</button></span>
-        <p>Order No: #45 </p>
-        <p>Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
+        <p className="order-no" >Order No: #45 </p>
+        <p className="order">Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
         <div>
-        <p>due : jun 30,2019 3:39 pm</p>
+          <div className="last-div">
+        <span className="time" > due : jun 30,2019 3:39 pm</span>
         <p>Assigned to <span></span></p>
+          </div>
         </div>
       </div>
       <div>
         <span>#25 <button>Response due</button></span>
-        <p>Order No: #201 </p>
-        <p>Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
-        <div>
-        <p>due : may 30,2019 3:39 pm</p>
+        <p className="order-no">Order No: #201 </p>
+        <p className="order">Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
+        <div className="last-div">
+        <span className="time">due : may 30,2019 3:39 pm</span>
         <p>Assigned to <span></span></p>
         </div>
       </div>
       <div>
         <span>#24 <button>Response due</button></span>
-        <p>Order No: #1 </p>
-        <p>Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
-        <div>
-        <p>due : apr 10,2019 3:39 pm</p>
+        <p className="order-no">Order No: #1 </p>
+        <p className="order">Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
+        <div className="last-div">
+        <span className="time">due : apr 10,2019 3:39 pm</span>
         <p>Assigned to <span></span></p>
         </div>
       </div>
       <div>
         <span>#23 <button>Response due</button></span>
-        <p>Order No: #600 </p>
-        <p>Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
-        <div>
-        <p>due : jan 03,2019 3:39 am</p>
+        <p className="order-no">Order No: #600 </p>
+        <p className="order">Paratha Side DishParatha Side DishParatha Side DishParatha Side Dish</p>
+        <div className="last-div" >
+        <span className="time">due : jan 03,2019 3:39 am</span>
         <p>Assigned to <span></span></p>
         </div>
       </div>
